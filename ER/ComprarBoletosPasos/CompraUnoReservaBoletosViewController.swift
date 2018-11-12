f99a2173134adc5b25ccf608c3d2903f3f4b5583
tabeli <tabeli.acs@gmail.com>
2018-11-12 17:29:28 -0600

    Added back button in all views

---
+++ ER/ComprarBoletosPasos/CompraUnoReservaBoletosViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CompraUnoReservaBoletosViewController.swift
+//  ER
+//
+//  Created by Tabatha Acosta on 11/12/18.
+//  Copyright © 2018 Tabatha Acosta. All rights reserved.
+//
+
+import UIKit
+
+class CompraUnoReservaBoletosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
